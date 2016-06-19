274a1883e75f596ae5392769e800ff593ce5e22a
Remus Poienar <remuspoienar@yahoo.com>
2016-06-19 22:24:12 +0000

    Override devise views

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -10,13 +10,14 @@ module ApplicationCable
     protected
       def find_verified_user
         current_user = User.find_by(id: cookies.signed['user.id'])
-        cookie_not_expired = cookies.signed['user.expires_at'] > Time.now
         
-        if current_user && cookie_not_expired
-          current_user
-        else
-          reject_unauthorized_connection
+        if current_user
+          cookie_not_expired = cookies.signed['user.expires_at'] > Time.now
+          
+          return current_user if cookie_not_expired
         end
+        
+        reject_unauthorized_connection
       end
   end
 end
